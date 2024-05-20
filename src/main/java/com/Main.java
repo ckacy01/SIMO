e5950987dcd43d4debfd43faa4fdc7a4c9eb5da2
xGeorgeZ <114208215+xGeorgeZ@users.noreply.github.com>
2024-05-19 19:34:41 -0700

    Update Main.java

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,1 +1,20 @@
+/**
+ * Main.java
+ * 
+ * Punto de entrada de la aplicación.
+ * Inicia la vista principal de la aplicación de escritorio.
+ * 
+ * Autor: [Avila Carrillo Jorge Armando]
+ * Fecha de creación: [19 - MAYO - 2024]
+ */
 
+package com.tuempresa.tuapp;
+
+import com;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+        mainView.display();
+    }
+}
