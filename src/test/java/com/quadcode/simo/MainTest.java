fb13efdee4cfa27951d1fbe068241a155e50ba7e
xGeorgeZ <jorgesr10@outlook.com>
2024-06-09 12:36:06 -0700

    Cambio de organizacion de carpetas. Reviewed-by: xGeorgeZ

---
+++ src/test/java/com/quadcode/simo/MainTest.java
@@ -0,0 +1,28 @@
+/**
+ * MainTest.java
+ * 
+ * Punto de entrada de la aplicación.
+ * Inicia la vista principal de la aplicación de escritorio.
+ * 
+ * Autor: [Avila Carrillo Jorge Armando]
+ * Fecha de creación: [19 - MAYO - 2024]
+ */
+
+package com.quadcode.simo;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainTest extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("SIMO TEST");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+      launch(args);
+    }
+}
