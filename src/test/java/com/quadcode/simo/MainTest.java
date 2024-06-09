10efdb62d7c369ac3cf10541452f65e8acb0c361
xGeorgeZ <jorgesr10@outlook.com>
2024-06-09 12:37:49 -0700

    Cambio de organizacion de carpetas. Reviewed-by: xGeorgeZ

--- src/test/java/com/quadcode/simo/MainTest.java
+++ src/test/java/com/quadcode/simo/MainTest.java
@@ -1,9 +1,7 @@
 /**
  * MainTest.java
- * 
  * Punto de entrada de la aplicación.
  * Inicia la vista principal de la aplicación de escritorio.
- * 
  * Autor: [Avila Carrillo Jorge Armando]
  * Fecha de creación: [19 - MAYO - 2024]
  */
