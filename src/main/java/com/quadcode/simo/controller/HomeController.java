e21443cb84defe5c4fc355d9fccf140150f1dfac
xGeorgeZ <jorgesr10@outlook.com>
2024-07-24 18:04:07 -0700

    Create HomeController.java

---
+++ src/main/java/com/quadcode/simo/controller/HomeController.java
@@ -0,0 +1,12 @@
+/**
+ * HomeController.java
+ * Se creara la logica de la pagina principal Home
+ * Sus funciones, que hace cada boton y como funciona la interfaz en si
+ * Author: [Avila Carrillo Jorge Armando]
+ * Fecha de creación: [24 - Julio - 2024]
+ */
+
+package com.quadcode.simo.controller;
+
+public class HomeController {
+}
